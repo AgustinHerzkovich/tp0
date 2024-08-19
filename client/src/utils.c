10090e3287251effe8058e2ab4e2fc089ea559ed
AgustinHerzkovich <aherzkovich@frba.utn.edu.ar>
2024-08-19 01:04:31 +0000

    Correcciones utils

--- client/src/utils.c
+++ client/src/utils.c
@@ -32,7 +32,7 @@ int crear_conexion(char *ip, char* puerto)
 	int socket_cliente = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
 
 	// Ahora que tenemos el socket, vamos a conectarlo
-	int err = connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen);
+	connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen);
 
 	freeaddrinfo(server_info);
 
